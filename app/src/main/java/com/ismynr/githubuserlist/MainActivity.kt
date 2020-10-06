ceb3d1350358d43e953733958851bd3f7c71ac18
ismynr <gekkai7is.my@gmail.com>
2020-10-06 19:26:00 +0700

    ListView github user added!

--- app/src/main/java/com/ismynr/githubuserlist/MainActivity.kt
+++ app/src/main/java/com/ismynr/githubuserlist/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.ismynr.githubuserlist
 
+import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: UserAdapter
+    private lateinit var dataName: Array<String>
+    private lateinit var dataUsername: Array<String>
+    private lateinit var dataFollowers: Array<String>
+    private lateinit var dataFollowing: Array<String>
+    private lateinit var dataLocation: Array<String>
+    private lateinit var dataRepository: Array<String>
+    private lateinit var dataCompany: Array<String>
+    private lateinit var dataAvatar: TypedArray
+    private var users = arrayListOf<User>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView: ListView = findViewById(R.id.lv_user)
+        adapter = UserAdapter(this)
+        listView.adapter = adapter
+
+        prepare()
+        addItem()
+
     }
+
+    private fun prepare(){
+        dataName = resources.getStringArray(R.array.name)
+        dataUsername = resources.getStringArray(R.array.username)
+        dataFollowers = resources.getStringArray(R.array.followers)
+        dataFollowing = resources.getStringArray(R.array.following)
+        dataLocation = resources.getStringArray(R.array.location)
+        dataRepository = resources.getStringArray(R.array.repository)
+        dataCompany = resources.getStringArray(R.array.company)
+        dataAvatar = resources.obtainTypedArray(R.array.avatar)
+    }
+
+    private fun addItem(){
+        for (position in dataName.indices){
+            val user = User(
+                dataName[position],
+                dataUsername[position],
+                dataFollowers[position],
+                dataFollowing[position],
+                dataLocation[position],
+                dataRepository[position],
+                dataCompany[position],
+                dataAvatar.getResourceId(position, -1)
+            )
+            users.add(user)
+        }
+
+        adapter.users = users
+    }
+
 }
---
+++ app/src/main/java/com/ismynr/githubuserlist/User.kt
@@ -0,0 +1,12 @@
+package com.ismynr.githubuserlist
+
+data class User (
+    val name: String?,
+    val username: String?,
+    val followers: String?,
+    val following: String?,
+    val location: String?,
+    val repository: String?,
+    val company: String?,
+    val avatar: Int?
+)
